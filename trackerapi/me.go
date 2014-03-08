a32d5b7f12b3f0ee2035fcb879fb0fc89227c638
ecin <ecin@copypastel.com>
2014-03-08 14:14:43 -0800

    forking repo requires internal code changes

--- trackerapi/me.go
+++ trackerapi/me.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	u "os/user"
 
-	"github.com/GoBootcamp/clirescue/cmdutil"
-	"github.com/GoBootcamp/clirescue/user"
+	"github.com/ecin/clirescue/cmdutil"
+	"github.com/ecin/clirescue/user"
 )
 
 var (
